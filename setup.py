3fab02d89b3f45ce909dd0a3390a6484777cf88e
Mike Burr <miburr@cisco.com>
2014-11-28 06:25:09 +0900

    more setup() fields. version bump

--- setup.py
+++ setup.py
@@ -8,22 +8,25 @@ import direg
 with open('README.rst', 'w') as f:
     f.write(direg.__doc__)
 
+NAME = 'direg'
 
 def read(file):
     with open(file, 'r') as f:
         return f.read().strip()
 
 setup(
-    name='direg',
+    name=NAME,
     version=read('VERSION'),
     description=('A drop-in registry. Easily maintain a central configuration registry for your complex '
                  'python project.'),
-    long_description=read('README.rst'),
+    long_description='\n' + read('README.rst'),
     author='Mike Burr',
     author_email='mburr@unintuitive.org',
-    url='https://github.com/stnbu/direg',
-    download_url='https://github.com/stnbu/direg/archive/master.zip',
+    url='https://github.com/stnbu/{0}'.format(NAME),
+    download_url='https://github.com/stnbu/{0}/archive/master.zip'.format(NAME),
+    provides=[NAME],
     license='MIT',
+    bugtrack_url='https://github.com/stnbu/{0}/issues'.format(NAME),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -32,7 +35,7 @@ setup(
         'Topic :: Software Development',
         'Topic :: Utilities',
     ],
-    packages=['direg'],
+    packages=[NAME],
     keywords=['configuration', 'registry', 'refactor'],
     test_suite='nose.collector',
     test_requires=['nose'],
